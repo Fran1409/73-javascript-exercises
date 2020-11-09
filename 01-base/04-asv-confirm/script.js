b3cc0a53a913f6dbb1f8da586d018d65db6d08f9
Fran1409 <fran_andries@hotmail.com>
2020-11-09 11:29:40 +0100

    Add prompt and function in 04-asv-confirm

--- 01-base/04-asv-confirm/script.js
+++ 01-base/04-asv-confirm/script.js
@@ -11,6 +11,20 @@
 
 (function() {
 
-    // your code here
+    function getInformation() {
+        var age = prompt("What's your age?");
+        var gender = prompt("What's your gender?");
+        var town = prompt("In what town do you live?");
+
+        var check = confirm("Is this information correct? \nYour age is "+age+", your gender is "+gender+" and you live in the town "+town+".");
+
+        if (check == false) {
+            alert("Our information is incorrect, let's do it again.");
+            getInformation();
+        };
+
+    };
+    
+    getInformation()
 
 })();
