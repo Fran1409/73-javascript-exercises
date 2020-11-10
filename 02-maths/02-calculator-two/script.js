1663d75ead0fff6d258a35fe0d85f9cd303b710a
Fran1409 <fran_andries@hotmail.com>
2020-11-10 10:38:24 +0100

    Add variables and switch operation in 02-calculator-two

--- 02-maths/02-calculator-two/script.js
+++ 02-maths/02-calculator-two/script.js
@@ -10,10 +10,34 @@
 // You will have time to focus on it later.
 
 (() => {
-    // to get the value of an input: document.getElementById("element-id").value
+    
 
     const performOperation = operation => {
-        // perform the operation
+
+        let operatorone = parseFloat(document.getElementById("op-one").value);
+        let operatortwo = parseFloat(document.getElementById("op-two").value);
+
+        switch(operation) {
+            case 'addition':
+                let addition = operatorone + operatortwo;
+                alert("The answer is "+addition)
+                break;
+
+            case 'substraction':
+                let substraction = operatorone - operatortwo;
+                alert("The answer is "+substraction)
+                break;
+
+            case 'multiplication':
+                let multiplication = operatorone * operatortwo;
+                alert("The answer is "+multiplication)
+                break;
+
+            case 'division':
+                let division = operatorone / operatortwo;
+                alert("The answer is "+division)
+                break;
+        }
     };
 
     Array.from(document.querySelectorAll("button.operator")).forEach($btn =>
